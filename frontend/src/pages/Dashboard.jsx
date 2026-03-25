71870fa0a94ca447d1a6f4de594415445a4b7842
Sharon2507 <deviallipilli84@gmail.com>
2026-03-25 15:26:09 +0530

    updated

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,7 +3,9 @@ import Navbar from "../components/Navbar";
 function Dashboard() {
   return (
     <div style={{ padding: "40px" }}>
-      <h1 style={{ color: "black" }}>Welcome to Dashboard</h1>
+      <h1 style={{ color: "black" }}>Welcome,</h1>
+       <h1>Chat with your PDFs using AI</h1>
+    <p>Instantly get answers and insights from your PDF documents.</p>
     </div>
   );
 }
